c65040a654b9e02b0dd1a7eef433edac44b583ef
JasonGITHUB <Jasonvirtualai@gmail.com>
2025-04-03 23:24:27 -0700

    Fix mongoose config to use full Mongo URI

--- backend/db/mongooseConfig.js
+++ backend/db/mongooseConfig.js
@@ -3,18 +3,12 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    const dbName = process.env.DB_NAME; // Extract database name from environment variables
-    console.log(`Connecting to database: ${dbName}`);
-    await mongoose.connect(process.env.MONGODB_URL, {
-      //   useNewUrlParser: true,
-      //   useUnifiedTopology: true,
-      // dbName: process.env.DB_NAME,
-      dbName: dbName, // ✅ Use the loaded DB_NAME
-    });
-    console.log("MongoDB connected successfully");
+    console.log(`Connecting to MongoDB...`);
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("✅ MongoDB connected successfully");
   } catch (error) {
-    console.error("MongoDB connection error:", error);
-    process.exit(1); // Exit the process with failure
+    console.error("❌ MongoDB connection error:", error);
+    process.exit(1);
   }
 };
 
